da893a3c2a9e3d3fa231931517045f4fc2aedf27
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-05-28 16:14:12 -0300

    init: first commit

---
+++ src/main/java/br/com/matheus/mpstore/MpStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.mpstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheus/mpstore/MpStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.mpstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
