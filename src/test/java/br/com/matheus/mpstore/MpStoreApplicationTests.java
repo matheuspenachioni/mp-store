d188ae3073fc1029a2e37576aeb425031ba7cf00
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-08-07 03:01:29 -0300

    test: basic CRUD test

--- src/test/java/br/com/matheus/mpstore/MpStoreApplicationTests.java
+++ src/test/java/br/com/matheus/mpstore/MpStoreApplicationTests.java
@@ -1,13 +1,24 @@
 package br.com.matheus.mpstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MpStoreApplicationTests {
+import br.com.matheus.mpstore.controller.AcessoController;
+import br.com.matheus.mpstore.model.Acesso;
 
+@SpringBootTest(classes = MpStoreApplication.class)
+class MpStoreApplicationTests {
+	
+	@Autowired
+	private AcessoController acessoController;
+	
 	@Test
-	void contextLoads() {
+	void testeCadastraAcesso() {
+		Acesso acesso = new Acesso();
+		acesso.setDescricao("ROLE_ADMIN");
+		
+		acessoController.saveAcesso(acesso);
 	}
 
 }
